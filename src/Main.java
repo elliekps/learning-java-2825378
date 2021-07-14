9a52bfe033ded86a795578a7539dd6cabc1db8ae
elliekps <85143334+elliekps@users.noreply.github.com>
2021-07-14 15:57:15 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,28 @@
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String args[]) {
-        String question = "";
-        String choiceOne = "";
-        String choiceTwo = "";
-        String choiceThree = "";
+        String question = "What is 10 + 10?";
+        String choiceOne = "100";
+        String choiceTwo = "20";
+        String choiceThree = "1";
 
         String correctAnswer = choiceTwo;
+        
+        System.out.println(question);
+        System.out.println(choiceOne + "or" + choiceTwo + "or" + choiceThree + "?");
+        
+        Scanner input = new Scanner(System.in);
+        Answer = input.nextString();
 
+        if (correctAnswer = choiceTwo) {
+            System.out.println("Congratulations! You've got the correct answer.);
+        } else {
+           System.out.println("That is incorrect, the correct answer is 20.);
+        }
+    }
+              
         // Write a print statement asking the question
         // Write a print statement giving the answer choices
 
